d6051f158e12317078fe2858a5121f61145b624f
JaysM32 <71362967+JaysM32@users.noreply.github.com>
2023-04-05 16:34:02 +0700

    Add files via upload

--- training-tensor.py
+++ training-tensor.py
@@ -9,6 +9,9 @@ from keras.models import Model
 from keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
 import tensorflow as tf
 
+# Import metric calculations
+from keras.metrics import Precision, Recall
+
 # TESTING WITHOUT USING GPUs AS THIS LAPTOP ONLY HAVE INTEGRATED GPUs 
 # Avoid OOM errors by setting GPU Memory Consumption Growth
 #gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -21,11 +24,6 @@ POS_PATH = os.path.join('FaceRecog/tensorflow/data', 'positive')
 NEG_PATH = os.path.join('FaceRecog/tensorflow/data', 'negative')
 ANC_PATH = os.path.join('FaceRecog/tensorflow/data', 'anchor')
 
-# DONE LOCALLY, RUN ONCE ON NEW PLACES IF NOT MADE
-# Make the directories
-#os.makedirs(POS_PATH)
-#os.makedirs(NEG_PATH)
-#os.makedirs(ANC_PATH)
 
 # LFW tar file download link: http://vis-www.cs.umass.edu/lfw/
 
@@ -35,3 +33,219 @@ ANC_PATH = os.path.join('FaceRecog/tensorflow/data', 'anchor')
 #        EX_PATH = os.path.join('FaceRecog/tensorflow/lfw', directory, file)
 #        NEW_PATH = os.path.join(NEG_PATH, file)
 #        os.replace(EX_PATH, NEW_PATH)
+
+
+anchor = tf.data.Dataset.list_files(ANC_PATH+'\*.jpg').take(20)
+positive = tf.data.Dataset.list_files(POS_PATH+'\*.jpg').take(20)
+negative = tf.data.Dataset.list_files(NEG_PATH+'\*.jpg').take(20)
+
+dir_test = anchor.as_numpy_iterator()
+
+def preprocess(file_path):
+    
+    # Read in image from file path
+    byte_img = tf.io.read_file(file_path)
+    # Load in the image 
+    img = tf.io.decode_jpeg(byte_img)
+    
+    # Preprocessing steps - resizing the image to be 100x100x3
+    img = tf.image.resize(img, (100,100))
+    # Scale image to be between 0 and 1 
+    img = img / 255.0
+
+    # Return image
+    return img
+
+
+#dataset.map(preprocess)
+
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives)
+
+samples = data.as_numpy_iterator()
+exampple = samples.next()
+#print(exampple)
+
+def preprocess_twin(input_img, validation_img, label):
+    return(preprocess(input_img), preprocess(validation_img), label)
+
+res = preprocess_twin(*exampple)
+#print(res)
+
+#plt.imshow(res[1])
+
+
+# Build dataloader pipeline
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=1024)
+
+# Training partition
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+# Testing partition
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
+
+def make_embedding(): 
+    inp = Input(shape=(100,100,3), name='input_image')
+    
+    # First block
+    c1 = Conv2D(64, (10,10), activation='relu')(inp)
+    m1 = MaxPooling2D(64, (2,2), padding='same')(c1)
+    
+    # Second block
+    c2 = Conv2D(128, (7,7), activation='relu')(m1)
+    m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
+    
+    # Third block 
+    c3 = Conv2D(128, (4,4), activation='relu')(m2)
+    m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
+    
+    # Final embedding block
+    c4 = Conv2D(256, (4,4), activation='relu')(m3)
+    f1 = Flatten()(c4)
+    d1 = Dense(4096, activation='sigmoid')(f1)
+    
+    
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
+
+embedding = make_embedding()
+#embedding.summary()
+
+class L1Dist(Layer):
+    
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
+    
+l1 = L1Dist()
+
+def make_siamese_model(): 
+    
+    # Anchor image input in the network
+    input_image = Input(name='input_img', shape=(100,100,3))
+    
+    # Validation image in the network 
+    validation_image = Input(name='validation_img', shape=(100,100,3))
+    
+    # Combine siamese distance components
+    siamese_layer = L1Dist()
+    siamese_layer._name = 'distance'
+    distances = siamese_layer(embedding(input_image), embedding(validation_image))
+    
+    # Classification layer 
+    classifier = Dense(1, activation='sigmoid')(distances)
+    
+    return Model(inputs=[input_image, validation_image], outputs=classifier, name='SiameseNetwork')
+
+siamese_model = make_siamese_model()
+
+binary_cross_loss = tf.losses.BinaryCrossentropy()
+opt = tf.keras.optimizers.Adam(1e-4) # 0.0001
+
+#checkpoints
+checkpoint_dir = './FaceRecog/tensorflow/training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+checkpoint = tf.train.Checkpoint(opt=opt, siamese_model=siamese_model)
+
+#tests
+test_batch = train_data.as_numpy_iterator()
+batch_1 = test_batch.next()
+
+
+#training
+def train_step(batch):
+    
+    # Record all of our operations 
+    with tf.GradientTape() as tape:     
+        # Get anchor and positive/negative image
+        X = batch[:2]
+        # Get label
+        y = batch[2]
+        
+        # Forward pass
+        yhat = siamese_model(X, training=True)
+        # Calculate loss
+        loss = binary_cross_loss(y, yhat)
+    print(loss)
+        
+    # Calculate gradients
+    grad = tape.gradient(loss, siamese_model.trainable_variables)
+    
+    # Calculate updated weights and apply to siamese model
+    opt.apply_gradients(zip(grad, siamese_model.trainable_variables))
+        
+    # Return loss
+    return loss
+
+def train(data, EPOCHS):
+    # Loop through epochs
+    for epoch in range(1, EPOCHS+1):
+        print('\n Epoch {}/{}'.format(epoch, EPOCHS))
+        progbar = tf.keras.utils.Progbar(len(data))
+        
+        # Creating a metric object 
+        r = Recall()
+        p = Precision()
+        
+        # Loop through each batch
+        for idx, batch in enumerate(data):
+            # Run train step here
+            loss = train_step(batch)
+            yhat = siamese_model.predict(batch[:2])
+            r.update_state(batch[2], yhat)
+            p.update_state(batch[2], yhat) 
+            progbar.update(idx+1)
+        print(f"loss {loss.numpy()}, recall {r.result().numpy()}, percision {p.result().numpy()}")
+        
+        # Save checkpoints  
+        if epoch % 2 == 0: 
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+EPOCHS = 10
+train(train_data, EPOCHS)
+
+
+# Get a batch of test data
+test_input, test_val, y_true = test_data.as_numpy_iterator().next()
+y_hat = siamese_model.predict([test_input, test_val])
+
+r = Recall()
+p = Precision()
+
+for test_input, test_val, y_true in test_data.as_numpy_iterator():
+    yhat = siamese_model.predict([test_input, test_val])
+    r.update_state(y_true, yhat)
+    p.update_state(y_true,yhat) 
+
+print(r.result().numpy(), p.result().numpy())
+
+print(y_true)
+
+# Set plot size 
+plt.figure(figsize=(10,8))
+
+# Set first subplot
+plt.subplot(1,2,1)
+plt.imshow(test_input[0])
+
+# Set second subplot
+plt.subplot(1,2,2)
+plt.imshow(test_val[0])
+
+# Renders cleanly
+plt.show()
+
+# Save weights
+siamese_model.save('siamesemodelinitialtesting.h5')
